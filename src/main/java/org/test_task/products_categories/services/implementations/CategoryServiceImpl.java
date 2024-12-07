514fd8757717583567d43e67f8d56d44d566790a
pankoivan <panivan98@yandex.ru>
2024-12-07 18:58:52 +0300

    Начал реализацию сервисов категорий и продуктов

--- src/main/java/org/test_task/products_categories/services/implementations/CategoryServiceImpl.java
+++ src/main/java/org/test_task/products_categories/services/implementations/CategoryServiceImpl.java
@@ -1,4 +1,48 @@
 package org.test_task.products_categories.services.implementations;
 
-public class CategoryServiceImpl {
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.test_task.products_categories.dto.in.category.CategoryAddingDto;
+import org.test_task.products_categories.dto.in.category.CategoryEditingDto;
+import org.test_task.products_categories.entities.Category;
+import org.test_task.products_categories.repositories.CategoryRepository;
+import org.test_task.products_categories.services.interfaces.CategoryService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Setter(onMethod_ = @Autowired)
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository repository;
+
+    @Override
+    public Category findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Category add(CategoryAddingDto addingDto, BindingResult bindingResult) {
+        return null;
+    }
+
+    @Override
+    public Category edit(CategoryEditingDto editingDto, BindingResult bindingResult) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
 }
--- src/main/java/org/test_task/products_categories/services/implementations/ProductServiceImpl.java
+++ src/main/java/org/test_task/products_categories/services/implementations/ProductServiceImpl.java
@@ -1,4 +1,48 @@
 package org.test_task.products_categories.services.implementations;
 
-public class ProductServiceImpl {
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.test_task.products_categories.dto.in.product.ProductAddingDto;
+import org.test_task.products_categories.dto.in.product.ProductEditingDto;
+import org.test_task.products_categories.entities.Product;
+import org.test_task.products_categories.repositories.ProductRepository;
+import org.test_task.products_categories.services.interfaces.ProductService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Setter(onMethod_ = @Autowired)
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository repository;
+
+    @Override
+    public Product findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Product add(ProductAddingDto addingDto, BindingResult bindingResult) {
+        return null;
+    }
+
+    @Override
+    public Product edit(ProductEditingDto editingDto, BindingResult bindingResult) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
 }
