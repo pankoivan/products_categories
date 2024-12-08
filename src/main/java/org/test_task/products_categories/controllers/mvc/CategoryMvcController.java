db0da384749602cc85bab99d74c4cbb12c933f04
pankoivan <panivan98@yandex.ru>
2024-12-08 13:00:57 +0300

    В черновом варианте реализовал MVC-контроллеры категорий и товаров

--- src/main/java/org/test_task/products_categories/controllers/mvc/CategoryMvcController.java
+++ src/main/java/org/test_task/products_categories/controllers/mvc/CategoryMvcController.java
@@ -1,4 +1,32 @@
 package org.test_task.products_categories.controllers.mvc;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.test_task.products_categories.services.interfaces.CategoryService;
+
+@Controller
+@RequestMapping("/categories")
+@AllArgsConstructor
+@PreAuthorize("permitAll()")
 public class CategoryMvcController {
+
+    private final CategoryService service;
+
+    @GetMapping
+    public String findAll(Model model) {
+        model.addAttribute("categories", service.findAll());
+        return "categories";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(Model model, @PathVariable("id") String pathId) {
+        model.addAttribute("category", service.findById(service.validateAndParsePathId(pathId)));
+        return "category";
+    }
+
 }
--- src/main/java/org/test_task/products_categories/controllers/mvc/ProductMvcController.java
+++ src/main/java/org/test_task/products_categories/controllers/mvc/ProductMvcController.java
@@ -1,4 +1,32 @@
 package org.test_task.products_categories.controllers.mvc;
 
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.test_task.products_categories.services.interfaces.ProductService;
+
+@Controller
+@RequestMapping("/products")
+@AllArgsConstructor
+@PreAuthorize("permitAll()")
 public class ProductMvcController {
+
+    private final ProductService service;
+
+    @GetMapping
+    public String findAll(Model model) {
+        model.addAttribute("products", service.findAll());
+        return "products";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(Model model, @PathVariable("id") String pathId) {
+        model.addAttribute("product", service.findById(service.validateAndParsePathId(pathId)));
+        return "product";
+    }
+
 }
